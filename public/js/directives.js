bc6221f1efeec08e96f5293b70248137bf2bcdad
John Tomaselli <jrt@gtz.com>
2013-04-05 19:32:16 -0300

    Update directives.js

--- public/js/directives.js
+++ public/js/directives.js
@@ -8,4 +8,29 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  
+.directive('layout', function() {
+    return {
+
+        link: function(scope, elm, attrs) {
+            var layout = elm.layout({ applyDefaultStyles: true });
+            console.log('layout ',layout)
+            scope.layout  = layout;
+
+            scope.$watch(attrs.state, function(state) {
+                if (state == true) {
+                    scope.layout.sizePane('east', 120);
+                    scope.layout.show('west');
+                    scope.layout.show('south');
+                } else {
+                    scope.layout.sizePane('east', 60);
+                    scope.layout.hide('west');
+                    scope.layout.hide('south');
+                }
+            });
+        }
+    };
+})
+  
+  ;
