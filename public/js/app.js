0a4543151b200e3ccd0bf0f8cffb405c81456b1f
John Tomaselli <jrt@gtz.com>
2013-04-10 16:36:04 -0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,14 @@
 
 
 // Declare app level module which depends on filters, and services
-// var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-var app = angular.module('myApp', [,'myApp.filters' ,'myApp.services','myApp.directives',  'ui'   ,'ui.bootstrap']).
+//angular.module('myApp', ['myApp.directives', 'myApp.services', 'ui'   ,'ui.bootstrap','myApp.filters'])
+// ,'ui.bootstrap'        ,'ui
 
-
-    config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider.
-    when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1}).
-    when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2}).
-    when('/faq',   {templateUrl:'partials/faq', controller:FaqCtrl}).
-    when('/select2',   {templateUrl:'partials/select2', controller:Select2Ctrl}).
-    otherwise({redirectTo: '/view1'});
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives' ,'ui'  ]).
+    config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
+    // $routeProvider.when('/faq', {        templateUrl:'partials/faq',        controller:FaqCtrl });
+    $routeProvider.otherwise({redirectTo: '/view1'});
     $locationProvider.html5Mode(true);
-  }]);
+}]);
